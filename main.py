fcc6b0e19c9b5b653758390ded0d456c53db871f
Aashutosh Dhakal <153347190+Aashutosh-Dhakal@users.noreply.github.com>
2024-08-31 18:29:51 +0545

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from pdf2image import convert_from_path, convert_from_bytes
+import tempfile
+import os
+
+pdf_folder_path = './pdf'
+file_names = os.listdir(pdf_folder_path)
+file_names = [f for f in file_names if os.path.isfile(
+    os.path.join(pdf_folder_path, f))]
+
+print('Program Started')
+try:
+    print('Starting Convertion...')
+    for each in file_names:
+        with tempfile.TemporaryDirectory() as path:
+            images_from_path = convert_from_path(
+                f'pdf/{each}', output_folder='./images')
+    print('Successfully Converted.')
+except:
+    print('Sorry there was an error encounterd.')
